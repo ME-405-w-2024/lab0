8a1f94ed25aecc5dc5ec14900fa8ca215034819a
jracers <43837350+jracers@users.noreply.github.com>
2024-01-11 17:52:53 -0800

    Updated sleep

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 
 import pyb
 import time
-import step_response as sr
+#import step_response as sr
 
 #Setup Pin C0
 pinC0 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP)
@@ -11,9 +11,9 @@ while 1:
     #Set value to 1
     pinC0.value(1)
     #Wait 5 sec
-    time.sleep(5)
-    sr.step_response()
+    time.sleep(1)
+    #sr.step_response()
     #Set value to 0
     pinC0.value(0)
     #Wait 5 sec
-    time.sleep(5)
+    time.sleep(1)
