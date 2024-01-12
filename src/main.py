f474609f4bf3a3fea5bc2cfe19965a1b3d3a57e4
jracers <43837350+jracers@users.noreply.github.com>
2024-01-12 14:48:43 -0800

    Added pip reqs

--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
 import pyb
+import utime
 
 def tick(timer):                # we will receive the timer object when being called
-    print(timer.counter())      # show current timer's counter value
+    print('Tick')      # show current timer's counter value
 tim = pyb.Timer(4, freq=2)      # create a timer object using timer 4 - trigger at 1Hz
 tim.callback(tick)              # set the callback to our tick function
+
+#Setup Pin C0
+pinC0 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP)
+
+#Loop Forever
+while 1:
+    #Set value to 1
+    pinC0.value(1)
+    #Wait 5 sec
+    utime.sleep(5)
+    #Set value to 0
+    pinC0.value(0)
+    #Wait 5 sec
+    utime.sleep(5)
